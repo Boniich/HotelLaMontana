174a658b8eaa45ba456afb1726bd89db2cd08a53
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-01 18:28:25 -0300

    changes related to auth authentication

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import LoginView from './components/views/login/LoginView';
 import { Routes, Route } from "react-router-dom";
 import Home from './components/views/home/Home';
 import Others from './components/views/others/Others';
+import ProtectedHome from './router/ProtectedHome';
+import Layout from './components/views/Layout/Layout';
 
 function App() {
   return (
-    <div className="App container">
         <Routes>
-            <Route path="/" element={<LoginView />}/>
-            <Route path="/home" element={<Home/>}/>
-            <Route path="/others" element={<Others />}/>
+          <Route path="/" element={<Layout />}>
+
+            {/* Publick */}
+            <Route path="login" element={<LoginView />}/>
+
+            {/* Protected */}
+            <Route element={<ProtectedHome/>}>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/others" element={<Others />}/>
+            </Route>
+            </Route>
         </Routes>
-    </div>
   );
 }
 
