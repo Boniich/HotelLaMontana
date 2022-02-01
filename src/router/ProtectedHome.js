249bed740c5c0580414c8184fe881154329a474e
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-01 18:31:20 -0300

    handle the access if the token exists

---
+++ src/router/ProtectedHome.js
@@ -0,0 +1,11 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+
+const ProtectedHome = () =>{
+    
+    const location = useLocation();
+
+    return(
+        localStorage.getItem("token") ? <Outlet/>: <Navigate to="/login" state={{from: location}} replace/>
+    )
+}
+export default ProtectedHome;
