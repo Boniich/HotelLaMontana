7cef8e792e3d94aa3f287d1afea160a72d33da17
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-03 10:17:50 -0300

    a small title and description of hotel

---
+++ src/components/views/login/hotelTitle/HotelTitle.js
@@ -0,0 +1,10 @@
+export const HotelTitle = () =>{
+
+    return(
+        <div className='title-description'>
+
+        <h1>Hotel La Montaña</h1>
+        <p>Disfruta de unas increibles vacaciones</p>
+    </div>
+    )
+}
