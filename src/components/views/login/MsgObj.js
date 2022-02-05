381eb872c95a5ddda32a6a3b812aa9f6549ba14b
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-05 16:16:35 -0300

    contains obj of SweetAlert

---
+++ src/components/views/login/MsgObj.js
@@ -0,0 +1,26 @@
+
+export let loadingMsg = {
+    title: "Procesando informacion",
+    text: "Espere por favor",   
+    allowOutsideClick: false,
+    showConfirmButton: false,
+}
+
+export let emptyFields = {
+    icon: 'warning',
+    title: 'Campos de Email y password estan vacios',
+    text: 'Por favor, complete los campos!',
+}
+
+export let notResponse ={
+    icon: 'error',
+    title: 'oops...',
+    text: 'No fue posible conectar con el servidor, por favor intentelo tarde',
+}
+
+export let wrongData = {
+    icon: 'warning',
+    title: 'Email y/o password no son correctos',
+    text: 'Por favor, verifique sus datos!',
+}
+
