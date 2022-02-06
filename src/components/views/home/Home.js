b47e86015af156d0b98514e8e3cdcce79f081195
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-06 11:01:01 -0300

    added css link

---
+++ src/components/views/home/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Home.css"
+import { TitleAndDescription } from "../../common/titleAndDescription/TitleAndDescription";
+import Menu from "./menu/Menu";
+
+export default function Home(){
+    return(
+        <div className="container">
+            <TitleAndDescription 
+                title="Home" 
+                description="Selecciona la comida perfecta para ti!"
+            />
+            <section className="flex-box">
+                <Menu/>
+            </section>
+        </div>
+    )
+}
