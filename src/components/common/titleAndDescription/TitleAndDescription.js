f84e82e97489146c012d2ec4d25ad16504acba02
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-06 09:32:03 -0300

    added re-used component for title and description

---
+++ src/components/common/titleAndDescription/TitleAndDescription.js
@@ -0,0 +1,9 @@
+export const TitleAndDescription = ({title,description}) =>{
+
+    return(
+        <div className='title-description'>
+        <h1>{title}</h1>
+        <p>{description}</p>
+    </div>
+    )
+}
