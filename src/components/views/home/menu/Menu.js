b63bcc7af64d8d19e517c26739fbb1de773a25cc
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-12 19:06:38 -0300

    getFood function is now a costumed hook and in the menu file there is the function that show and delete results by id of recipe

--- src/components/views/home/menu/Menu.js
+++ src/components/views/home/menu/Menu.js
@@ -4,39 +4,57 @@ import MenuView from "./MenuView";
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
 import { deleteMsg } from "./MsgObj";
+import { useFood } from "../../../../hooks/useFood";
 
 const MySwal = withReactContent(Swal);
 
 export default function Menu(){
 
-    const [food, setFood] = useState([]);
+    const [info, setInfo] = useState([]);
+
+    const {food} = useFood();
 
     useEffect(() =>{
-        getFood();
-        
-    },[])
+        getFoodInfo();
+    },[food])
 
-    async function getFood(){
-        console.log("food");
-        let url = "https://api.spoonacular.com/recipes/complexSearch?number=4&apiKey=ffe601b2c0bf40e99eca791908d30c41";
+    async function getFoodInfo(){
+        console.log("info");
+        console.log("food 2",food);
+        for(let e = 0; e<food.length;e++){
+            console.log("entro");
+            let url = `https://api.spoonacular.com/recipes/${food[e].id}/information?includeNutrition=false&apiKey=ffe601b2c0bf40e99eca791908d30c41`;
 
-        try{
-            const res = await axios.get(url);
-            console.log(res.data.results);
-            setFood(res.data.results);
+            try{
+                const res = await axios.get(url);
+                console.log(res);
 
-        }catch(err){
-            console.log(err);
+                let obj = {
+                    id: res.data.id,
+                    title: res.data.title,
+                    image: res.data.image,
+                    readyIn: res.data.readyInMinutes,
+                    price: res.data.pricePerServing,
+                    healtScore: res.data.healtScore
+                }
+
+                console.log("ID",obj);
+                setInfo((info) => [...info,obj]);
+            }catch(err){
+                console.log(err);
+            }
+
         }
     }
+
 
     const deleteFromMenu = (id) =>{
 
         MySwal.fire(deleteMsg).then((result) => {
             if (result.isConfirmed) {
 
-            let data = food.filter(el => el.id !== id);
-            setFood(data);
+            let data = info.filter(el => el.id !== id);
+            setInfo(data);
 
               MySwal.fire(
                 'Eliminado!',
@@ -49,13 +67,14 @@ export default function Menu(){
 
     return(
         <>
-        {food.map((el)=>(
+        {info.map((el)=>(
             <MenuView 
                 key={el.id} 
                 id={el.id}
                 title={el.title} 
                 image={el.image}
-                deleteFromMenu={deleteFromMenu}/>
+                deleteFromMenu={deleteFromMenu}
+                />
         ))}
         </>
 
