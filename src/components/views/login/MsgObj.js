87d4cfdc200c792dae818a01061e57ce10694c90
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-14 19:22:58 -0300

    obj for not server response was moved to anothe file for global use

--- src/components/views/login/MsgObj.js
+++ src/components/views/login/MsgObj.js
@@ -4,12 +4,6 @@ export let emptyFields = {
     text: 'Por favor, complete los campos!',
 }
 
-export let notResponse ={
-    icon: 'error',
-    title: 'oops...',
-    text: 'No fue posible conectar con el servidor, por favor intentelo tarde',
-}
-
 export let wrongData = {
     icon: 'warning',
     title: 'Email y/o password no son correctos',
