3885c506abcd6881ce039284f4f127f35040bba7
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-14 19:14:06 -0300

    obj are now consts and have his name in uppercase

--- src/consts/sweetAlertMsg/index.js
+++ src/consts/sweetAlertMsg/index.js
@@ -1,6 +1,12 @@
-export let loadingMsg = {
+export const LOADING_MSG = {
     title: "Procesando informacion",
     text: "Espere por favor",   
     allowOutsideClick: false,
     showConfirmButton: false,
+}
+
+export const NOT_RESPONSE_SERVER ={
+    icon: 'error',
+    title: 'oops...',
+    text: 'No fue posible conectar con el servidor, por favor intentelo tarde',
 }
