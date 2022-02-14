33729d9a0c3318c0051af3decf5e4c008c9956eb
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-14 19:06:48 -0300

    added loading msg and error msg when point of API go to 0

--- src/hooks/useFood.js
+++ src/hooks/useFood.js
@@ -1,28 +1,40 @@
 import { useEffect, useState } from "react"
 import axios from "axios";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import { loadingMsg } from "../consts/sweetAlertMsg";
+
+const MySwal = withReactContent(Swal);
+
+const CONECTION_ERROR_FOOD_API ={
+    icon: 'error',
+    title: 'oops...',
+    text: 'Es muy posible que se hayan agotado las peticiones a la API, por favor, itentalo mañana nuevamente',
+}
 
 export const useFood = () =>{
 
     const [food, setFood] = useState([]);
     console.log("entro en food");
     useEffect(() =>{
-
+        MySwal.fire(loadingMsg);
         getFood();
-
     },[]);
 
 
     async function getFood(){
         console.log("food");
-        let url = "https://api.spoonacular.com/recipes/complexSearch?number=4&apiKey=ffe601b2c0bf40e99eca791908d30c41";
+        let url = "https://api.spoonacular.com/recipes/complexSearch?number=4&aiKey=ffe601b2c0bf40e99eca791908d30c41";
 
         try{
             const res = await axios.get(url);
-            // console.log(res.data.results);
             setFood(res.data.results);
 
         }catch(err){
-            console.log(err);
+            if(err?.response.status === 402){
+                MySwal.fire(CONECTION_ERROR_FOOD_API);
+            }
+            
         }
     }
 
