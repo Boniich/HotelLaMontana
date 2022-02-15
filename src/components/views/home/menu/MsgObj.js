0fda07a02d6148836f1e43b7bd2797677575197f
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-15 10:45:05 -0300

    files deleted

--- src/components/views/home/menu/MsgObj.js
+++
@@ -1,9 +0,0 @@
-export const deleteMsg = {
-    title: 'Estas seguro de eliminar este plato?',
-    icon: 'warning',
-    showCancelButton: true,
-    confirmButtonColor: '#3085d6',
-    cancelButtonColor: '#d33',
-    confirmButtonText: 'Si, Eliminalo!',
-    cancelButtonText: "Cancelar",
-}
