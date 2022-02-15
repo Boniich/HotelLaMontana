a0775908fd4e858a3f2411f27af715b86bc1c0df
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-15 10:36:24 -0300

    changed the MsgObj for sweetAlertLoginObj and changed the let obj for a const obj and names in uppercase

---
+++ src/components/views/login/sweetAlertLoginObj.js
@@ -0,0 +1,12 @@
+export const EMPTY_FIELDS = {
+    icon: 'warning',
+    title: 'Campos de Email y password estan vacios',
+    text: 'Por favor, complete los campos!',
+}
+
+export const WRONG_DATA = {
+    icon: 'warning',
+    title: 'Email y/o password no son correctos',
+    text: 'Por favor, verifique sus datos!',
+}
+
