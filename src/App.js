108f655bf557a7b3711dbd5820acb120d76c5432
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-16 10:49:45 -0300

    fixed the indentation with prettier

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import './App.css';
-import LoginView from './components/views/login/LoginView';
+import "./App.css";
+import LoginView from "./components/views/login/LoginView";
 import { Routes, Route } from "react-router-dom";
-import Home from './components/views/home/Home';
-import ProtectedHome from './router/ProtectedHome';
-import Layout from './components/views/layout/Layout';
+import Home from "./components/views/home/Home";
+import ProtectedHome from "./router/ProtectedHome";
+import Layout from "./components/views/layout/Layout";
 
 function App() {
   return (
-        <Routes>
-          <Route path="/" element={<Layout />}>
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        {/* Publick */}
+        <Route path="login" element={<LoginView />} />
 
-            {/* Publick */}
-            <Route path="login" element={<LoginView />}/>
-
-            {/* Protected */}
-            <Route element={<ProtectedHome/>}>
-              <Route path="/" element={<Home/>}/>
-            </Route>
-            </Route>
-        </Routes>
+        {/* Protected */}
+        <Route element={<ProtectedHome />}>
+          <Route path="/" element={<Home />} />
+        </Route>
+      </Route>
+    </Routes>
   );
 }
 
