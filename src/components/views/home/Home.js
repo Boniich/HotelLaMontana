8688d5611291ddd19905f5ab295c63ac7dda0b1f
Ezequiel Bonino <ezequieldbo22@hotmail.com>
2022-02-25 11:59:58 -0300

    added a component for render global values. Also the Home.js file is which handle the function and state that save the info for cards

--- src/components/views/home/Home.js
+++ src/components/views/home/Home.js
@@ -1,17 +1,88 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import { TitleAndDescription } from "../../common/titleAndDescription/TitleAndDescription";
+import axios from "axios";
+import { useFood } from "../../../hooks/useFood";
+import Swal from "sweetalert2";
+import withReactContent from "sweetalert2-react-content";
+import Scores from "./scores/Scores";
 import Menu from "./menu/Menu";
 
+const MySwal = withReactContent(Swal);
+
 export default function Home() {
+  let priceTotal = 0;
+  let readyInTotal = 0;
+  let healthScoreTotal = 0;
+
+  const [info, setInfo] = useState([]);
+  const [price, setPrice] = useState(0);
+  const [readyIn, setReadyIn] = useState(0);
+  const [healthScore, setHealthScore] = useState(0);
+
+  const { food } = useFood();
+
+  useEffect(() => {
+    getFoodInfo();
+  }, [food]);
+
+  async function getFoodInfo() {
+    for (let e = 0; e < food.length; e++) {
+      console.log("entro");
+      let url = `${process.env.REACT_APP_RECIPE_API}${food[e].id}/information?includeNutrition=false&${process.env.REACT_APP_API_KEY}`;
+
+      try {
+        const res = await axios.get(url);
+        console.log(res);
+
+        let obj = {
+          id: res.data.id,
+          title: res.data.title,
+          image: res.data.image,
+          readyIn: res.data.readyInMinutes,
+          price: res.data.pricePerServing,
+          healthScore: res.data.healthScore,
+        };
+        console.log(obj);
+
+        priceTotal += obj.price;
+        readyInTotal += obj.readyIn;
+        healthScoreTotal += obj.healthScore;
+
+        console.log("ID", obj);
+        setInfo((info) => [...info, obj]);
+
+        MySwal.close();
+      } catch (err) {
+        console.log(err);
+      }
+    }
+
+    setPrice(priceTotal);
+    setReadyIn(readyInTotal);
+    setHealthScore(healthScoreTotal);
+  }
+
   return (
     <div className="container">
       <TitleAndDescription
         title="Home"
         description="Selecciona la comida perfecta para ti!"
       />
+
+      <Scores price={price} readyIn={readyIn} healthScore={healthScore} />
+
       <section className="flex-box">
-        <Menu />
+        <Menu
+          info={info}
+          price={price}
+          readyIn={readyIn}
+          healthScore={healthScore}
+          setInfo={setInfo}
+          setPrice={setPrice}
+          setReadyIn={setReadyIn}
+          setHealthScore={setHealthScore}
+        />
       </section>
     </div>
   );
